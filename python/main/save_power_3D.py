94c2b30ce367060c2a3d61f4852e0e3fe188cd57
Keir Rogers <keir@hypatia-login.local>
2017-07-10 13:26:45 +0100

    Improved testing script for full pipeline

--- python/main/save_power_3D.py
+++ python/main/save_power_3D.py
@@ -19,29 +19,29 @@ if __name__ == "__main__":
     spectra_full_dir_path = sys.argv[5]
     model_cosmology_filename = sys.argv[6]
 
-    undodged_spectra_ins = box.SimulationBox(snapshot_num, snapshot_dir, grid_width, spectral_res, reload_snapshot=False, spectra_savedir=spectra_full_dir_path, spectra_savefile_root='gridded_spectra_DLAs_LLS_dodged')
-
-    #print(np.mean(np.exp(-1. * undodged_spectra_ins.get_optical_depth())))
+    undodged_spectra_ins = box.SimulationBox(snapshot_num, snapshot_dir, grid_width, spectral_res, reload_snapshot=False, spectra_savedir=spectra_full_dir_path, spectra_savefile_root='gridded_spectra_DLAs_dodged')
+    print(np.mean(np.exp(-1. * undodged_spectra_ins.get_optical_depth())))
 
     undodged_spectra_ins.convert_fourier_units_to_distance = True
     spectra_box = undodged_spectra_ins.skewers_realisation(mean_flux_specified=0.675940542622) #0.67573418185771716) #_hydrogen_overdensity(ion = -1)
     #spectra_box = undodged_spectra_ins.skewers_realisation(tau_scaling_specified = 1.5)
     k_box = undodged_spectra_ins.k_box()
     mu_box = undodged_spectra_ins.mu_box()
-    for i in np.arange(0.5,0.9,0.1):
+
+    for i in [1.]: #np.arange(0.5,0.9,0.1):
         print(i)
-        n_mu_bins = 4
+        n_mu_bins = 8
         n_k_bins = 6
         k_min = np.min(k_box[k_box > 0. / u.Mpc])
         k_max = i * 0.704 / u.Mpc #np.max(k_box)
         k_bin_max = mh.exp(mh.log(k_max.value) + ((mh.log(k_max.value) - mh.log(k_min.value)) / (n_k_bins - 1))) / u.Mpc
         k_bin_edges = np.exp(np.linspace(mh.log(k_min.value), mh.log(k_bin_max.value), n_k_bins + 1)) / u.Mpc
         #k_bin_edges[-2] = k_max
-        #mu_bin_edges = np.linspace(0., 1., n_mu_bins + 1)
-        mu_bin_edges = np.array([0., 0.5, 0.8, 0.95, 1.])
+        mu_bin_edges = np.linspace(0., 1., n_mu_bins + 1)
+        #mu_bin_edges = np.array([0., 0.5, 0.8, 0.95, 1.])
 
         fourier_estimator_instance = fou.FourierEstimator3D(spectra_box)
         #power,df_hat = fourier_estimator_instance.get_flux_power_3D()
         power_binned, k_binned, mu_binned, bin_counts = fourier_estimator_instance.get_flux_power_3D_two_coords_hist_binned(k_box,np.absolute(mu_box),k_bin_edges,mu_bin_edges,bin_coord2=True,std_err=False)
 
-        np.savez('/home/keir/Data/Illustris_big_box_spectra/snapdir_064/power_DLAs_LLS_dodged_64_750_10_4_6_kMax_%.2f.npz'%i,power_binned,k_binned,bin_counts,mu_binned) #,model_power_binned)'''
+        np.savez('/home/keir/Data/Illustris_big_box_spectra/snapdir_064/power_DLAs_dodged_64_750_10_8_6_kMax_%.2f.npz'%i,power_binned,k_binned,bin_counts,mu_binned,k_box.value) #,model_power_binned)'''
--- python/main/test_inference.py
+++ python/main/test_inference.py
@@ -1,10 +1,14 @@
+# -*- coding: utf-8 -*-
+
 import numpy as np
 import astropy.units as u
 import math as mh
 import sys
 
+import power_spectra as pos
 import boxes as box
 import fourier_estimators as fou
+import utils as uti
 
 if __name__ == "__main__":
     model_cosmology_filename = sys.argv[1]
@@ -12,16 +16,20 @@ if __name__ == "__main__":
 
     #Input parameters
     box_size = {'x': 106.5 * u.Mpc, 'y': 106.5 * u.Mpc, 'z': 106.5 * u.Mpc} # = 75 Mpc / h
-    n_samp = {'x': 751, 'y': 751, 'z': 751}
+    n_samp = {'x': 21, 'y': 21, 'z': 21}
     redshift = 2.44
     H0 = (70.4 * u.km) / (u.s * u.Mpc)
     omega_m = 0.2726
-    n_mu_bins = 4
-    n_k_bins = 6
+    n_mu_bins = 8
+    n_k_bins = 15
 
     #Input anisotropic functional form
     def mu_coefficients(k_para, k_perp):
-        return np.array([0. * k_para.value, 0. * k_para.value, 0. * k_para.value, 0. * k_para.value, 1. + 0. * k_para.value]) #Isotropic limit
+        #return np.array([0. * k_para.value, 0. * k_para.value, 0. * k_para.value, 0. * k_para.value, 1. + 0. * k_para.value]) #Isotropic limit
+        b = -0.122
+        beta = 1.663
+        #return np.array([0. * k_para.value, 0. * k_para.value, 0. * k_para.value, 0. * k_para.value, (b**2) + (0. * k_para.value)]) #Isotropic biased limit
+        return np.array([((b**2) * (beta**2)) + (0 * k_para.value), 0. * k_para.value, (2. * beta * (b**2)) + (0. * k_para.value), 0. * k_para.value, (b**2) + (0. * k_para.value)]) #Anisotropic biased limit
 
     #Gaussian box instances
     test_gaussian_ins = box.GaussianBox(box_size, n_samp, redshift, H0, omega_m)
@@ -36,16 +44,28 @@ if __name__ == "__main__":
     k_max = k_max = 0.704 / u.Mpc #np.max(k_box)
     k_bin_max = mh.exp(mh.log(k_max.value) + ((mh.log(k_max.value) - mh.log(k_min.value)) / (n_k_bins - 1))) / u.Mpc
     k_bin_edges = np.exp(np.linspace(mh.log(k_min.value), mh.log(k_bin_max.value), n_k_bins + 1)) / u.Mpc
-    mu_bin_edges = np.array([0., 0.5, 0.8, 0.95, 1.])
+    mu_bin_edges = np.linspace(0., 1., n_mu_bins + 1)
+    #mu_bin_edges = np.array([0., 0.5, 0.8, 0.95, 1.])
 
+    print('Here')
     #Gaussian boxes
-    test_gaussian_box = test_gaussian_ins.anisotropic_pre_computed_gauss_realisation(model_cosmology_filename, mu_coefficients)
-    np.save('/home/keir/Data/Illustris_big_box_spectra/snapdir_064/test_gaussian_box_isotropic_751_751_751.npy',test_gaussian_box)
+    test_gaussian_box = test_gaussian_ins.anisotropic_power_law_gauss_realisation(-3.,0.5 / u.Mpc,1.,mu_coefficients) #anisotropic_pre_computed_gauss_realisation(model_cosmology_filename, mu_coefficients)
+    print('Here2')
+    np.save('/home/keir/Data/Illustris_big_box_spectra/snapdir_064/test_gaussian_box_anisotropic_biased_minus3_power_21_21_21_num1.npy',test_gaussian_box)
+    #test_gaussian_box = np.load('/home/keir/Data/Illustris_big_box_spectra/snapdir_064/test_gaussian_box_isotropic_751_751_751.npy')
 
+    print('Here3')
     #Fourier estimator instances
     fourier_estimator_instance = fou.FourierEstimator3D(test_gaussian_box)
 
+    #Bin theory power spectra
+    power_spectrum_instance = pos.PowerLawPowerSpectrum(-3.,0.5 / u.Mpc,1.)
+    power_theory_box = power_spectrum_instance.evaluate3d_isotropic(k_box)
+    power_theory_binned = uti.bin_f_x_y_histogram(k_box.flatten()[1:],np.absolute(mu_box).flatten()[1:],power_theory_box.flatten()[1:],k_bin_edges,mu_bin_edges)
+
     #Power spectra
     power_bin,k_bin,mu_bin,bin_count = fourier_estimator_instance.get_flux_power_3D_two_coords_hist_binned(k_box,np.absolute(mu_box),k_bin_edges,mu_bin_edges,std_err=False)
+    #power_raw = fourier_estimator_instance.get_flux_power_3D()[0]
 
-    np.savez(save_filename, power_bin, k_bin, bin_count, mu_bin)
+    np.savez(save_filename, power_bin, k_bin, bin_count, mu_bin, power_theory_binned)
+    #np.savez(save_filename, power_raw, k_box.value, np.absolute(mu_box.value))
