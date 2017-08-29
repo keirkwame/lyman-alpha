30fd0d4bb7b9f162b433273d7e899035a03a5eb9
Keir Rogers <keir.rogers.14@ucl.ac.uk>
2017-08-29 20:12:27 +0100

    Improved Voigt fitting routine

--- python/main/get_voigts.py
+++ python/main/get_voigts.py
@@ -7,8 +7,9 @@ if __name__ == "__main__":
     box_instance = box.SimulationBox(64, '/Users/keir/Documents/lyman_alpha/simulations/illustris_big_box_spectra', 300, 25 * u.km / u.s, reload_snapshot=False, spectra_savefile_root='gridded_spectra', spectra_savedir='/Users/keir/Documents/lyman_alpha/simulations/illustris_big_box_spectra/snapdir_064')
     #_smallDLAs_forest
     optical_depth = box_instance.get_optical_depth()
+    column_density = box_instance.get_column_density()
 
     #voigt_output = vf.get_voigt_systems(optical_depth[0], 25)
-    voigt_instance = vf.Profiles(optical_depth[23424], 25)
-    voigt_instance.do_fit(tol = 1.e-2)
+    voigt_instance = vf.Profiles(optical_depth[0], 25) #23424
+    voigt_instance.do_fit() #tol = 1.e-1, signif = 0.5, sattau = 30)
     voigt_output = voigt_instance.get_fitted_profile()
