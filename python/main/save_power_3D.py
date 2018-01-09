921ddcaac513382539aeb42090206438f8e7a9fe
Keir Rogers <keir@hypatia-login.local>
2018-01-09 18:46:55 +0000

    Minor change before merge

--- python/main/save_power_3D.py
+++ python/main/save_power_3D.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     spectra_full_dir_path = sys.argv[5]
     model_cosmology_filename = sys.argv[6]
 
-    undodged_spectra_ins = box.SimulationBox(snapshot_num, snapshot_dir, grid_width, spectral_res, reload_snapshot=False, spectra_savedir=spectra_full_dir_path, spectra_savefile_root='gridded_spectra_LLS_forest')
+    undodged_spectra_ins = box.SimulationBox(snapshot_num, snapshot_dir, grid_width, spectral_res, reload_snapshot=False, spectra_savedir=spectra_full_dir_path, spectra_savefile_root='gridded_spectra') #_LLS_forest')
     #print(np.mean(np.exp(-1. * undodged_spectra_ins.get_optical_depth())))
 
     undodged_spectra_ins.convert_fourier_units_to_distance = True
@@ -50,6 +50,8 @@ if __name__ == "__main__":
         fourier_estimator_instance = fou.FourierEstimator3D(spectra_box)
         #power,df_hat = fourier_estimator_instance.get_flux_power_3D()
         #power_binned, k_binned, mu_binned, bin_counts = fourier_estimator_instance.get_flux_power_3D_two_coords_hist_binned(k_box,np.absolute(mu_box),k_bin_edges,mu_bin_edges,bin_coord2=True,std_err=False)
-        correlation_binned, r_binned, mu_binned, bin_counts = fourier_estimator_instance.get_correlation_function_3D_two_coords_hist_binned(r_box, np.absolute(mu_r_box), r_bin_edges, mu_bin_edges)
+        #correlation_binned, r_binned, mu_binned, bin_counts = fourier_estimator_instance.get_correlation_function_3D_two_coords_hist_binned(r_box, np.absolute(mu_r_box), r_bin_edges, mu_bin_edges)
+        correlation = fourier_estimator_instance.get_correlation_function_3D()
 
-        np.savez('/home/keir/Data/Illustris_big_box_spectra/snapdir_064/correlation_LLS_forest_64_750_10_4_6_evenMu_kMax_%.2f.npz'%i,correlation_binned,r_binned,bin_counts,mu_binned) #,model_power_binned)'''
+        #np.savez('/home/keir/Data/Illustris_big_box_spectra/snapdir_064/correlation_LLS_forest_64_750_10_4_6_evenMu_kMax_%.2f.npz'%i,correlation_binned,r_binned,bin_counts,mu_binned) #,model_power_binned)'''
+        np.savez('/home/keir/Data/Illustris_big_box_spectra/snapdir_064/correlation_total_64_25_25_unbinned.npz',correlation,r_box.value,mu_r_box.value)
